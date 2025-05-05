2b5566518f8a660c7db025f1f25d3cd5f0e3a9c4
KFCMan <leonardo.a.t.m.05@gmail.com>
2025-05-04 22:32:23 -0600

    NumberInput on PartTags

---
+++ src/lib/components/NumberInput.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    type Props = {
+        label: string;
+        value: number;
+    };
+
+    let { label, value = $bindable(1) }: Props = $props();
+</script>
+
+<label>
+    <p>{label}</p>
+    <input type="number" name="" bind:value />
+</label>
+
+<style>
+    label {
+        display: inline-block;
+        margin-inline: auto;
+    }
+</style>
--- src/lib/components/PartTag.svelte
+++ src/lib/components/PartTag.svelte
@@ -2,6 +2,7 @@
     import { RMPosition, type RobotMaster } from "$lib/data/robotMasters.js";
     import type { BaseImageKey } from "$lib/image/PartComponents.svelte.js";
     import { getCanvasControllerContext } from "./context.js";
+    import NumberInput from "./NumberInput.svelte";
     import RmSelectTag from "./RMSelectTag.svelte";
 
     type Props = {
@@ -31,32 +32,44 @@
     }
 </script>
 
-<article>
+<article class="flex">
     <img bind:this={image} alt={`${rmName}'s ${rmPart}`} />
     <div>
         <p>{`${rmName}'s ${rmPart}`}</p>
         <RmSelectTag bind:value={rmName} />
     </div>
+    <div>
+        <NumberInput label="X Position" value={0} />
+        <NumberInput label="Y Position" value={0} />
+    </div>
 </article>
 
 <style>
-    article {
+    .flex {
         display: flex;
         flex-direction: row;
         flex-wrap: nowrap;
         align-items: center;
+    }
 
+    .flex > * {
+        margin-inline: 0.5rem;
+    }
+
+    article {
         border-radius: 0.5rem;
         background-color: dimgray;
         color: white;
-    }
-
-    article > * {
-        margin-inline: 0.5rem;
+        padding: 0.5rem;
     }
 
     img {
         width: 2rem;
+        max-height: 3rem;
         image-rendering: pixelated;
+    }
+
+    p {
+        margin-block: 0.5rem;
     }
 </style>
