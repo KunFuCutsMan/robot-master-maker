afe6772a26de0a42489d1b96b1ae14fe923b5f09
KFCMan <leonardo.a.t.m.05@gmail.com>
2025-05-22 11:09:35 -0600

    CSS Styling for OpenDialog

--- src/lib/components/OpenDialog.svelte
+++ src/lib/components/OpenDialog.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
-    import { store, type DialogStore } from "$lib/data/dialogStore.svelte.js";
+    import {
+        closeDialog,
+        store,
+        type DialogStore,
+    } from "$lib/data/dialogStore.svelte.js";
     import { onMount, type Snippet } from "svelte";
 
     let inner = $state<Snippet | null>(null);
@@ -18,14 +22,39 @@
 </script>
 
 <dialog bind:this={dialog}>
-    {#if inner}
-        {@render inner()}
-    {/if}
+    <section>
+        {#if inner}
+            {@render inner()}
+        {/if}
+    </section>
+    <button aria-label="Close Dialog" class="surface-1" use:closeDialog
+        >X</button
+    >
 </dialog>
 
 <style>
     dialog {
         margin: auto;
-        padding: auto;
+        padding: var(--size-6);
+        position: relative;
+        inset: 0;
+
+        border: var(--size-1) solid var(--surface-3);
+        border-radius: var(--radius-3);
+    }
+
+    dialog::backdrop {
+        background-color: rgba(22, 25, 29, 0.75);
+    }
+
+    button {
+        position: absolute;
+        top: var(--size-1);
+        right: var(--size-1);
+        width: var(--size-4);
+        aspect-ratio: var(--ratio-square);
+        border: none;
+        border-radius: var(--radius-2);
+        cursor: pointer;
     }
 </style>
