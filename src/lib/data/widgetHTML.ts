d966e3f7cd4a4a1378fb79bf3299bfefddfe037f
KFCMan <leonardo.a.t.m.05@gmail.com>
2025-06-24 11:30:50 -0600

    widget uses border image

--- src/lib/data/widgetHTML.ts
+++ src/lib/data/widgetHTML.ts
@@ -1,8 +1,8 @@
 
 export const widgetHTML = `
-<section style="border-sizing: border-box; --orange-500: #f66d1e; --blue-800: #162c33; --gray-200: #161717; --gray-500: #5a5e5c; --gray-700: #b3bcb7; --gray-900: #ffffff;">
+<section style="border-sizing: border-box; --orange-500: #f66d1e; --blue-800: #162c33; --gray-900: #ffffff;">
 <div style="display: flex; flex-direction: column; align-items: center; background-color: var(--blue-800); padding: 1rem; border-radius: 0.5rem; color: var(--gray-900);">
-    <div style="width: 10rem; height: 10rem; background-color: var(--gray-200); display: flex; justify-content: center; align-items: center; border-image-source: repeating-linear-gradient( 0deg, var(--gray-500), var(--gray-700), var(--gray-500), var(--gray-700), var(--gray-500), var(--gray-700), var(--gray-500), var(--gray-700), var(--gray-500), var(--gray-700), var(--gray-500), var(--gray-700), var(--gray-500), var(--gray-700), var(--gray-500), var(--gray-700)); border-image-width: 1rem; border-image-repeat: round; border-image-slice: 10%;">
+    <div style="width: 10rem; height: 10rem; display: flex; justify-content: center; align-items: center; image-rendering: pixelated;border-image-source: url(https://file.garden/Z85j0wcYZ0vCXjU9/robot%20master%20frame.png); border-image-width: 2rem; border-image-repeat: round; border-image-slice: 33.33% fill;">
         <img src="IMAGE_URL_HERE" alt="Robot Master" />
     </div>
     <div style="width: 24ch; text-align: center; font-family: monospace;">
