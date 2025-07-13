6a90574851b7804d67a752dce637df70880da6be
KFCMan <leonardo.a.t.m.05@gmail.com>
2025-07-13 10:18:20 -0600

    Configuring to export as a single file

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,25 @@
-import { sveltekit } from '@sveltejs/kit/vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [svelte()],
 	define: {
 		APP_VERSION: JSON.stringify(process.env.npm_package_version)
+	},
+	resolve: {
+		alias: {
+			$lib: '/src/lib'
+		}
+	},
+	build: {
+		lib: {
+			entry: resolve(__dirname, 'src/lib/index.ts'),
+			name: 'robot-master-maker',
+		},
+		rollupOptions: {
+
+		}
 	}
 });
